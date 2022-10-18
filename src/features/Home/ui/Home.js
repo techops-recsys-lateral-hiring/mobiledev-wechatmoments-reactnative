fb0a088d20956cf704ce552e27637171b6ca8b6e
Nikhil <nikhil.kamboj@thoughtworks.com>
2022-10-18 09:56:57 +0530

    Network library integrated

--- src/features/Home/ui/Home.js
+++ src/features/Home/ui/Home.js
@@ -7,6 +7,7 @@ import {
 } from "react-native";
 import { Navigation } from 'react-native-navigation';
 import Counter from "../../Counter/ui/Counter";
+import {getRequest,postRequest} from  "../../../network/Network";
 
 
 class Home extends Component {
@@ -16,7 +17,7 @@ class Home extends Component {
         return (
             <View style={styles.container}>
                 <View style={{ flexDirection: 'row', width: 200, justifyContent: 'space-around' }}>
-                    <TouchableOpacity onPress={() => this.navigateToCounterScreen()}>
+                    <TouchableOpacity onPress={() => this.postRequest()}>
                         <Text style={{ fontSize: 20 }}>Navigate To Counter</Text>
                     </TouchableOpacity>
                 </View>
@@ -27,6 +28,28 @@ class Home extends Component {
     navigateToCounterScreen =()=>{
         this.props.navigation.navigate('Counter')
     }
+
+    
+     successHandler = (data) => {
+        console.log(data)
+      }
+
+      errorHandler = (error)=>{
+        console.log(error)
+      }
+
+    getRequest = ()=>{
+        getRequest("https://jsonplaceholder.typicode.com/posts/1",this.successHandler,this.errorHandler)
+    }
+
+    postRequest = ()=>{
+        postRequest("https://jsonplaceholder.typicode.com/posts",this.successHandler,this.errorHandler,{
+            "title": "foo",
+            "body": "bar",
+            "userId": 1,
+          })
+    }
+
 }
 
 
