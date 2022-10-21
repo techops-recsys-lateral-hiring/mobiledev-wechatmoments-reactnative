a0de76fb85838428abd0f0350399aa24b11ade21
andreea-cozonac-tw <112179144+andreea-cozonac-tw@users.noreply.github.com>
2022-10-21 08:51:21 +0300

    Add typescript (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,19 @@
  * @flow strict-local
  */
 
-import React, { Component } from 'react';
+import React, { ReactElement } from 'react';
 import { Provider } from 'react-redux';
 import MainNavigation from './navigation/MainNavigation';
 
 import configureStore  from './reducers/store';
 
-
-
-
 const store = configureStore()
 
-
-
-
-class App extends Component {
-
-  render() {
-  return(
-<Provider store={store}>
-<MainNavigation />
-  </Provider>
-)
-
-  }
+export function App(): ReactElement {
+  return (
+    <Provider store={store}>
+      <MainNavigation />
+    </Provider>
+  )
 }
-  
-export default App
+
--- src/Math.js
+++
@@ -1,9 +0,0 @@
-export const add=(number1,number2)=>{
-
-    return number1+number2
-
-}
-
-export const subtract = (number1,number2)=>{
-    return number1-number2
-}
